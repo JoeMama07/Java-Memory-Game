fe710a2602db67da6251de9171662846a9253c90
Josh <josh.k@tad.sr>
2023-06-22 23:04:09 -0300

    feat: add seperate config file

---
+++ src/Config/Config.java
@@ -0,0 +1,8 @@
+package Config;
+
+public class Config {
+    // Creating ENV VARIABLES
+    public final String DB_URL = "jdbc:mysql://localhost:3306/memory_game";
+    public final String USER = "root";
+    public final String PASSWORD = "root";
+}
--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,11 @@
+import Config.Config;
 import UserDao.UserDao;
 
 public class Main {
-    // Creating ENV VARIABLES
-    static final String DB_URL = "jdbc:mysql://localhost:3306/memory_game";
-    static final String USER = "root";
-    static final String PASSWORD = "root";
-
     public static void main(String[] args) {
+        Config config = new Config();
         System.out.println("Welcome to the Memory game");
         UserDao userDao = new UserDao();
-        userDao.dbConnection(DB_URL, USER, PASSWORD);
+        userDao.dbConnection(config.DB_URL, config.USER, config.PASSWORD);
     }
 }
